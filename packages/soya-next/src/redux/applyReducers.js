b2f01823097484ff3be0456145c489c78afd454f
Ayush Choubey <ayush.choubey@traveloka.com>
2021-09-08 00:18:18 +0800

    random indentation to initiate Merge conflict

--- packages/soya-next/src/redux/applyReducers.js
+++ packages/soya-next/src/redux/applyReducers.js
@@ -9,37 +9,37 @@ export default reducers => Component => {
     return Component;
   }
 
-  function ApplyReducers(props) {
-    const flag = React.useRef(true);
-    const context = React.useContext(ReactReduxContext);
-    const store = props.store || context.store;
+          function ApplyReducers(props) {
+            const flag = React.useRef(true);
+            const context = React.useContext(ReactReduxContext);
+            const store = props.store || context.store;
 
-    if (flag.current) {
-      if (!store.soya) {
-        throw new Error(
-          "applyReducers must be used with Soya's redux enhancer"
-        );
-      }
-      store.addReducer(reducers);
-      flag.current = false;
-    }
+            if (flag.current) {
+              if (!store.soya) {
+                throw new Error(
+                  "applyReducers must be used with Soya's redux enhancer"
+                );
+              }
+              store.addReducer(reducers);
+              flag.current = false;
+            }
 
-    return (
-      <Component {...props} />
-    );
-  }
+            return (
+              <Component {...props} />
+            );
+          }
   ApplyReducers.displayName = getDisplayName("applyReducers", Component);
-  ApplyReducers.getInitialProps = async (ctx) => {
-    if (!ctx.store.soya) {
-      throw new Error(
-        "applyReducers must be used with Soya's redux enhancer"
-      );
-    }
-    ctx.store.addReducer(reducers);
-    return (
-      Component.getInitialProps && (await Component.getInitialProps(ctx))
-    );
-  }
+                  ApplyReducers.getInitialProps = async (ctx) => {
+                    if (!ctx.store.soya) {
+                      throw new Error(
+                        "applyReducers must be used with Soya's redux enhancer"
+                      );
+                    }
+                    ctx.store.addReducer(reducers);
+                    return (
+                      Component.getInitialProps && (await Component.getInitialProps(ctx))
+                    );
+                  }
 
   return hoistStatics(ApplyReducers, Component, NEXT_STATICS);
 };
